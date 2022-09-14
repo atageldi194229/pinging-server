27982f51913ab3a88c2738d37cafb16a19a57749
atageldi194229 <atageldi194229@gmail.com>
2022-09-15 01:54:42 +0500

    update

--- scheduler.js
+++ scheduler.js
@@ -4,14 +4,13 @@ const INTERVAL = 2 * 1000;
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GITHUB_USERNAME = "atageldi194229";
 const GITHUB_REPO = "pinging-server";
+const WORKFLOW_ID = 34892269;
 
 const run = () => {
   axios
     .post(
-      `https://api.github.com/repos/${GITHUB_USERNAME}/${GITHUB_REPO}/dispatches`,
-      {
-        event_type: "hello",
-      },
+      `https://api.github.com/repos/${GITHUB_USERNAME}/${GITHUB_REPO}/actions/workflows/${WORKFLOW_ID}/dispatches`,
+      { ref: "main" },
       {
         headers: {
           Authorization: "Bearer: " + GITHUB_TOKEN,
