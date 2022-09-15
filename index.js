693c663ee772b965751c17ff4f2065f0c2a519e5
atageldi194229 <atageldi194229@gmail.com>
2022-09-16 02:11:38 +0500

    update

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-// const fs = require("fs");
-// const axios = require("axios");
-// const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
 
-// const today = new Date().toISOString().substring(0, 10);
+const today = new Date().toISOString().substring(0, 10);
 
-// const dir = path.join(__dirname, "data");
-// const file = path.join(__dirname, "data", "db.json");
-// const dateFile = path.join(__dirname, "data", today + ".json");
-// const fileListPath = path.join(__dirname, "data", "files.json");
+const dir = path.join(__dirname, "data");
+const hostListFile = path.join(__dirname, "data", "db.json");
+const todayJsonFile = path.join(__dirname, "data", today + ".json");
+const fileListPath = path.join(__dirname, "data", "files.json");
 
-// const URL = process.env.URL;
+const URL = process.env.URL;
 
-// if (!URL) {
-//   new Date().toISOString().substring(0, 10);
-//   console.log("URL is not defined");
-//   process.exit(1);
-// }
+if (!URL) {
+  new Date().toISOString().substring(0, 10);
+  console.log("URL is not defined");
+  process.exit(1);
+}
 
-// const getHost = (e) => `${e.ip}:${e.port}`;
+const getHost = (e) => `${e.ip}:${e.port}`;
 
-// const readFile = (filePath, defaultValue) => {
-//   if (fs.existsSync(filePath)) {
-//     return require(filePath);
-//   }
+const readFile = (file, defaultValue) => {
+  return fs.existsSync(file) ? require(file) : defaultValue;
+};
 
-//   return defaultValue;
-// };
+const writeHostsToFiles = (data) => {
+  let allHosts = readFile(hostListFile, []);
+  let newHosts = data.map((e) => !allHosts.includes(getHost(e)));
 
-// const writeHostsToFiles = (data) => {
-//   let all = readFile(file, []);
-//   let newAdded = readFile(dateFile, []); // today added hosts
+  for (let e of data) {
+    if (!allHosts.includes(getHost(e))) {
+      newHosts.push(e);
+    }
+  }
 
-//   let hosts = all.map(getHost);
+  fs.writeFileSync(
+    hostListFile,
+    JSON.stringify([...allHosts, ...newHosts.map(getHost)])
+  );
 
-//   for (let e of data) {
-//     if (!hosts.includes(getHost(e))) {
-//       all.push(e);
-//       newAdded.push(e);
-//     }
-//   }
+  // add new hosts to todays file
+  let all = readFile(todayJsonFile, []);
+  fs.writeFileSync(todayJsonFile, JSON.stringify([...all, ...newHosts]));
 
-//   fs.writeFileSync(file, JSON.stringify(all));
+  let regex = /[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].json/gm;
+  let files = fs.readdirSync(__dirname).filter((e) => regex.test(e));
+  fs.writeFileSync(fileListPath, JSON.stringify(files));
+};
 
-//   all = fs.existsSync(dateFile) ? require(dateFile) : [];
+axios.get(URL).then((res) => {
+  if (res.status !== 200) return;
 
-//   fs.writeFileSync(dateFile, JSON.stringify([...all, ...newAdded]));
-// };
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
 
-// axios.get(URL).then((res) => {
-//   if (res.status !== 200) return;
-
-//   if (!fs.existsSync(dir)) {
-//     fs.mkdirSync(dir, { recursive: true });
-//   }
-
-//   writeHostsToFiles(res.data);
-// });
+  writeHostsToFiles(res.data);
+});
