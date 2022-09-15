9bf893304d6d4d5e9a523c57cd54b99d4c25315e
atageldi194229 <atageldi194229@gmail.com>
2022-09-16 01:48:28 +0500

    bug

--- index.js
+++ index.js
@@ -2,19 +2,50 @@ const fs = require("fs");
 const axios = require("axios");
 const path = require("path");
 
+const today = new Date().toISOString().substring(0, 10);
+
 const dir = path.join(__dirname, "data");
 const file = path.join(__dirname, "data", "db.json");
+const dateFile = path.join(__dirname, "data", today + ".json");
+const fileListPath = path.join(__dirname, "data", "files.json");
 
-const URL =
-  process.env.URL ||
-  "https://duralga-next-vercel.vercel.app/api/key_req?key=123&deviceId=qwerty";
+const URL = process.env.URL;
 
 if (!URL) {
+  new Date().toISOString().substring(0, 10);
   console.log("URL is not defined");
   process.exit(1);
 }
 
 const getHost = (e) => `${e.ip}:${e.port}`;
+
+const readFile = (filePath, defaultValue) => {
+  if (fs.existsSync(filePath)) {
+    return require(filePath);
+  }
+
+  return defaultValue;
+};
+
+const writeHostsToFiles = (data) => {
+  let all = readFile(file, []);
+  let newAdded = readFile(dateFile, []); // today added hosts
+
+  let hosts = all.map(getHost);
+
+  for (let e of data) {
+    if (!hosts.includes(getHost(e))) {
+      all.push(e);
+      newAdded.push(e);
+    }
+  }
+
+  fs.writeFileSync(file, JSON.stringify(all));
+
+  all = fs.existsSync(dateFile) ? require(dateFile) : [];
+
+  fs.writeFileSync(dateFile, JSON.stringify([...all, ...newAdded]));
+};
 
 axios.get(URL).then((res) => {
   if (res.status !== 200) return;
@@ -23,24 +54,5 @@ axios.get(URL).then((res) => {
     fs.mkdirSync(dir, { recursive: true });
   }
 
-  let db = [];
-  let hosts = [];
-
-  if (fs.existsSync(file)) {
-    db = require(file);
-
-    for (let e of db) {
-      hosts.push(getHost(e));
-    }
-  }
-
-  const { data } = res;
-
-  for (let e of data) {
-    if (!hosts.includes(getHost(e))) {
-      db.push(e);
-    }
-  }
-
-  fs.writeFileSync(file, JSON.stringify(db, null, 2));
+  writeHostsToFiles(res.data);
 });
